ce53279ee276532e0809a75201fb0c9b23bd33aa
CR-Boo <42726540+CraftsionBoo@users.noreply.github.com>
2024-03-15 14:09:45 +0800

    Update download.sh

--- assets/download.sh
+++ assets/download.sh
@@ -4,4 +4,6 @@
 # yolov3
 wget -c https://pjreddie.com/media/files/darknet53.conv.74
 # yolov4 
-wget -c 
+wget -c https://pjreddie.com/media/files/yolov4.conv.137
+# yolov4-tiny
+wget -c https://pjreddie.com/media/files/yolov4-tiny.conv.29
